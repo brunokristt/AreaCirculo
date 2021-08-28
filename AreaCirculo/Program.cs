b0dd8caec8ee991f47bc9c13d115764edf3c67d3
brunokristt <brunokristt@gmail.com>
2021-08-28 16:57:48 -0300

    Adicionar arquivos de projeto.

---
+++ AreaCirculo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+ 
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de área de circulos");
+            Console.Write("Digite o raio do circulo: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double CalculaArea(double raio)
+            {
+                double resultado = 3.14159 * raio * raio;
+                return resultado;
+            }
+
+            double area = CalculaArea(raio);
+
+            Console.WriteLine("O valor da área do círculo é: " + area.ToString("F4"), CultureInfo.InvariantCulture);
+        }
+    }
+}
